048645b1b140b197f2a4fbe686be2a863f019f4b
Edley Forestal <edleyforestal@outlook.com>
2024-10-17 15:08:36 -0400

    Initial server setup and api endpoint test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios"
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const fetchAPI = async () => {
+    const res = await axios.get("http://localhost:8080/api")
+    console.log(res.data.dummy)
+  }
+
+  useEffect(() => {
+    fetchAPI()
+  },[])
 
   return (
     <>
