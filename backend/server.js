9184fbe1e71d289d6886677f1f55da7936224612
Edley Forestal <edleyforestal@outlook.com>
2024-10-17 17:56:58 -0400

    Redid cors, setup page routing

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,8 @@ const mongoose = require("mongoose")
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const corsOptions = {
-    origin: ["http://localhost:5173"]
-}
 
-app.use(cors(corsOptions))
+app.use(cors({origin: '*'}))
 
 app.get("/api", (req, res) => {
     res.json({"dummy": ["testing", "the", "backend"]})
