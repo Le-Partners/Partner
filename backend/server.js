b8fc94ed8bc93fedbb6125213101f58a961385b7
Nathan King <nateking0345@gmail.com>
2024-10-24 15:08:41 -0400

    set up database schemas

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,99 @@ dbMongo.once('open', function() {
   console.log('Connected to MongoDB');
 });
 
+const userSchema = new mongoose.Schema ({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    birthday:{
+        type: Date,
+        required: true,
+    },
+    gender:{
+        type:String,
+        required: true
+    },
+    bio:{
+        type : String,
+        required : true
+    }
+
+    
+});
+
+const postSchema = new mongoose.Schema({
+
+
+    username:{
+        type:String,
+        required: true,
+    },
+    message:{
+        type:String,
+        required: true
+    },
+    date_time:{
+        type:Date,
+        required: false
+    }
+});
+
+const partnerSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        required: true
+        
+    },
+    partners: {
+        type: [String],
+        required: true
+    }
+
+});
+
+const messageSchema = new mongoose.Schema({
+    sender_username:{
+        type: String,
+        required: true
+    },
+    reciever_username:{
+        type:String,
+        required: true
+    }, 
+    text_message:{
+        type: String,
+        required: true
+    },
+    is_read:{
+        type: Boolean,
+        required: true,
+        default: false
+    }
+});
+
+const notificationSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    notification_message:{
+        type: String,
+        required: true
+    },
+    is_read:{
+        type: Boolean,
+        required: true
+    }
+
+});
+
+
+
+
+
+
 app.get("/api", (req, res) => {
     res.json({"dummy": ["testing", "the", "backend"]})
 })
